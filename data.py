e0ac0b6ee2015359fb25e816d6a9474d477040c3
rdilip <9993319+rdilip@users.noreply.github.com>
2021-06-29 03:54:14 -0700

    Added additional qubits option

--- data.py
+++ data.py
@@ -7,7 +7,7 @@ import numpy as np
 import tensorflow as tf
 import tensorflow_datasets as tfds
 
-from compress import to_mps
+from compress import to_mps, pad_to_umps
 
 Batch = Mapping[str, np.ndarray]
 
@@ -41,6 +41,7 @@ def process_img(batch: Batch,
     x = batch['image'].astype(jnp.float32)
     assert (resize[0] * resize[1]) % 2 == 0
     x = x / 255.
+    L = np.prod(resize)
     
     if kernel is None:
         kernel = lambda x: jnp.concatenate([1-x, x], axis=-1)
@@ -48,12 +49,30 @@ def process_img(batch: Batch,
     nb, _, _, nc = x.shape
     x = np.array(jax.image.resize(x, [nb,resize[0],resize[1],nc], "nearest"))
     
+    # TODO: add option for kernel on top of added qubits -- Fourier?
     if compress:
-        img = [img.reshape(np.prod(resize)) for img in x]
-        img = pad_to_umps(to_mps(im, compress_kwargs['chi']) for im in img])
+        chi = compress_kwargs['chi']
+        x = x.reshape((nb, L))
+        img = [pad_to_umps(to_mps(v, chi_max=chi)) for v in x]
+        img = jnp.array(img)
+        mode = compress_kwargs.getdefault("mode", None)
+        qa = compress_kwargs.getdefault("add_qubits", 0)
+        nb, L, d, chi, _ = img.shape
+
+        if mode == "interleave":
+            img_qa = jnp.zeros((nb,(qa+1)*L,d,chi,chi))
+            img_qa = jax.ops.index_update(img_qa, (slice(nb),slice(0,(qa+1)*L,(qa+1))), img)
+            A = jnp.array([jnp.eye(chi)]*d)
+
+            for i in range(qa):
+                img_qa = jax.ops.index_update(img_qa, (slice(nb),slice(i+1,(qa+1)*L,qa+1)), A)
+            img = img_qa
+        elif mode == "append":
+            A = jnp.array([jnp.eye(chi)]*d*qa*nb).reshape((nb,qa,d,chi,chi))
+            img = jnp.concatenate((img, A), axis=1)
     else:
         img = kernel(x)
-        img = img.reshape((nb, 2, resize[0]*resize[1]//2, 2))
+        img = img.reshape((nb, L, img.shape[-1]))
 
     return {
         "image": img,
